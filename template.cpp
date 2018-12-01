8f997826521c7bfc27363f8e3620fa4fc71ae9ed
Yuzo Saito <yuzo.saito387@gmail.com>
2018-11-30 22:07:11 -0500

    update template

--- template.cpp
+++ template.cpp
@@ -4,10 +4,7 @@ using namespace std;
 // #define int long long
 struct Fast {Fast(){std::cin.tie(0);ios::sync_with_stdio(false);}} fast;
 
-#define _overload3(_1,_2,_3,name,...) name
-#define _rep(i,n) repi(i,0,n)
-#define repi(i,a,b) for(int i=int(a);i<int(b);++i)
-#define rep(...) _overload3(__VA_ARGS__,repi,_rep,)(__VA_ARGS__)
+#define rep(n) for (int i = 0; i < n; ++i)
 
 #define all(x) (x).begin(),(x).end()
 
@@ -66,8 +63,11 @@ int dy[4]={0,1,0,-1};
 #define UNIQUE(v) v.erase( unique(v.begin(), v.end()), v.end() );
 
 const int MOD = 1e9 + 7;
+const int INF = INT_MAX;
 
 signed main() {
+    Fast();
+
     return 0;
 }
 
